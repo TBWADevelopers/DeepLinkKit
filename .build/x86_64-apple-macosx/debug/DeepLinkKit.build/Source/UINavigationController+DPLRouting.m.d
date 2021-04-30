2f202ef895d0616dbb847cd16b180b9e2729eeb3
Vadim Pavlov <vadim.pavlov@tbwamobile.com>
2021-04-30 16:52:24 +0300

    Single folder for files

---
+++ .build/x86_64-apple-macosx/debug/DeepLinkKit.build/Source/UINavigationController+DPLRouting.m.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/vadym/Projects/DeepLinkKit/DeepLinkKit/Source/UINavigationController+DPLRouting.m \
+  /Users/vadym/Projects/DeepLinkKit/DeepLinkKit/Source/UINavigationController+DPLRouting.h \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX11.3.sdk/usr/include/mach-o/module.modulemap
